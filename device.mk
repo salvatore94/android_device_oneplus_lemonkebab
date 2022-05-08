9423d52978811f1a4b0a1a3e106f6280114195f8
dlwlrma123 <alexfinhart@gmail.com>
2022-05-08 19:53:30 +0800

    lemonkebab: bring back Renderscript * it is still being used on 12L and is spamming a lot, also we don't have the privilege to have a stable experience with Vulkan. * the purpose of deprecation is porting renderscript code to vulkan, which is unfortunately, unstable on our usecases.

--- device.mk
+++ device.mk
@@ -492,6 +492,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
